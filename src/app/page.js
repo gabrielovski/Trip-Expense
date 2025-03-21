2cac701ae06dd68de0d1d54795678f4f07dd886c
gabrielovski <gabriel8.cavalcante@gmail.com>
2025-03-20 21:31:20 -0300

    add home page

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Trip-Expense!</h1>
+    </div>
+  );
+}
