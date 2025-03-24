d43602bb20318dbeef78b67d88c618cfbb46c959
gabrielovski <gabriel8.cavalcante@gmail.com>
2025-03-24 19:45:25 -0300

    add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" className={GeistSans.className}>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
